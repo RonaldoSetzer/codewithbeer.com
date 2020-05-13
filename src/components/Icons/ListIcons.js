1468dee5bf5ec919d8ea707530f1066109469d2d
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-05-13 17:42:13 -0300

    feat: Create ListIcons to access all icons reference

---
+++ src/components/Icons/ListIcons.js
@@ -0,0 +1,27 @@
+import {
+  FaFolderOpen,
+  FaSearch,
+  FaSpotify,
+  FaTwitter,
+  FaGithub,
+  FaLinkedin,
+  FaHome,
+  FaFile,
+  FaRegFile,
+  FaChevronDown,
+} from 'react-icons/fa';
+
+const ListIcons = {
+  Folder: FaFolderOpen,
+  Home: FaHome,
+  Search: FaSearch,
+  Spotify: FaSpotify,
+  Twitter: FaTwitter,
+  Github: FaGithub,
+  Linkedin: FaLinkedin,
+  FileOutline: FaRegFile,
+  File: FaFile,
+  ChevronDown: FaChevronDown,
+};
+
+export default ListIcons;
