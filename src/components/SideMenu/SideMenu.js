e37cc116411e92d4bb0dfe17b189bb216abe9cc9
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-05-13 21:58:44 -0300

    feat(SideMenu): Add titles

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SocialLinks from '../SocialLinks';
 import FolderLinks from '../FolderLinks';
-import { Container, Content } from './styles';
+import { Container, Content, Chevron, Title, Link } from './styles';
 import { pages, series, categories } from './content';
 
 function SideMenu() {
@@ -10,6 +10,19 @@ function SideMenu() {
     <Container>
       <SocialLinks />
       <Content>
+        <Title>
+          <Chevron />
+          PROFILE
+        </Title>
+        <Link to="/about">
+          RONALDO SANTIAGO
+          <br />
+          Senior Developer
+        </Link>
+        <Title>
+          <Chevron />
+          CODE • WITH • BEER
+        </Title>
         <FolderLinks title={pages.title} links={pages.links} />
         <FolderLinks title={series.title} links={series.links} />
         <FolderLinks title={categories.title} links={categories.links} />
--- src/components/SideMenu/styles.js
+++ src/components/SideMenu/styles.js
@@ -1,4 +1,32 @@
 import styled from 'styled-components';
+import { Link as GLink } from 'gatsby';
+
+import Icons from '../Icons/ListIcons';
+
+const { ChevronDown } = Icons;
+
+export const Chevron = styled(ChevronDown)`
+  color: #ccc;
+  padding: 0.5rem;
+`;
+
+export const Link = styled(GLink)`
+  color: #ccc;
+  text-decoration: none;
+  padding: 0.5rem 1.2rem;
+
+  &:hover {
+    background-color: var(--links-highlight);
+  }
+`;
+
+export const Title = styled.h2`
+  background-color: var(--background-light);
+  color: #ccc;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+`;
 
 export const Container = styled.aside`
   display: flex;
