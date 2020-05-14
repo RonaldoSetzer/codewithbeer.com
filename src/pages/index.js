668917f031227dac343143d32694d133efb09868
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-05-13 21:15:51 -0300

    chore(add): Add dummy pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import GlobalStyles from '../styles/global';
 
-const IndexPage = () => (
-  <Layout>
-    <GlobalStyles />
-    <SEO title="Home" />
-    <h1>Code With Beer</h1>
-  </Layout>
-);
+function IndexPage() {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query PostList {
+        allMarkdownRemark {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date(locale: "en-us", formatString: "MMMM Do YYYY, h:mm a'")
+                description
+                category
+              }
+              timeToRead
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const postList = allMarkdownRemark.edges;
+
+  return (
+    <Layout>
+      <GlobalStyles />
+      <SEO title="Home" />
+      <h1>Code With Beer</h1>
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { category, date, description, title },
+          },
+        }) => (
+          <div>
+            <title>{title}</title>
+            {date}
+            {description}
+            {category}
+          </div>
+        )
+      )}
+    </Layout>
+  );
+}
 
 export default IndexPage;
