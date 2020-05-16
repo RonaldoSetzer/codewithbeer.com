6e036d1506691ea7b4bb9948d13af24653745fb5
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-05-16 15:25:10 -0300

    feat: Integrate Layout to Blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,13 +2,19 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Layout from '../components/Layout';
+import GlobalStyles from '../styles/global';
+import SEO from '../components/SEO';
+
 function BlogPost({ data }) {
   const { frontmatter, html } = data.markdownRemark;
   return (
-    <>
+    <Layout>
+      <GlobalStyles />
+      <SEO title={frontmatter.title} />
       <h1>{frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: html }} />
-    </>
+    </Layout>
   );
 }
 
