5c00abf8d4312e5513a4417e77a3b87a4c1ac6b3
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-05-31 02:03:03 -0300

    feat: Handle MenuButton click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Link } from './styles';
 import Icons from '../Icons/ListIcons';
 import Logo from '../Logo';
 
-function Header() {
+function Header({ isMenuOpen, setIsMenuOpen }) {
   const { Menu } = Icons;
+
+  const handleMenuOpen = e => {
+    e.preventDefault();
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Container>
       <Link to="/">
@@ -18,9 +25,14 @@ function Header() {
           <span>(Developer)</span>
         </Link>
       </div>
-      <Menu onClick={() => {}} />
+      <Menu onClick={handleMenuOpen} />
     </Container>
   );
 }
+
+Header.propTypes = {
+  isMenuOpen: PropTypes.bool.isRequired,
+  setIsMenuOpen: PropTypes.func.isRequired,
+};
 
 export default Header;
