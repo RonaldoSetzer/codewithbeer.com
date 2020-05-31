d2538f2595f89dd4b886e644d6de0b19084088d3
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-05-31 02:01:56 -0300

    feat: Handle isMenuOpen state on Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import SideMenu from '../SideMenu';
 import SocialLinks from '../SocialLinks';
+import Header from '../Header';
 
 import { Container, Main } from './styles';
 
 function Layout({ children }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <Container>
       <SocialLinks />
-      <SideMenu />
+      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <SideMenu isMenuOpen={isMenuOpen} />
       <Main>{children}</Main>
     </Container>
   );
