7a4f246b5c3e5f11315e7877151acef7d2e9fc5e
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-06-07 23:07:59 -0300

    feat: Handle with undefined date on Title

--- src/components/PostTitle/PostTitle.js
+++ src/components/PostTitle/PostTitle.js
@@ -3,16 +3,18 @@ import React from 'react';
 import { Container, Time } from './styles';
 
 function PostTitle({ date, title }) {
-  const [day, month, year] = date.split(' ');
+  const [day, month, year] = date ? date.split(' ') : [0, 0, 0];
   return (
     <Container>
-      <Time>
-        <span>{day}</span>
-        <div>
-          <p>{month}</p>
-          <p>{year}</p>
-        </div>
-      </Time>
+      {date && (
+        <Time>
+          <span>{day}</span>
+          <div>
+            <p>{month}</p>
+            <p>{year}</p>
+          </div>
+        </Time>
+      )}
       <h1>{title}</h1>
     </Container>
   );
