2fe8a8aad1dc3009fa3ed8d2065680e55af1e7ba
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-06-07 23:16:11 -0300

    feat: Apply Layout to 404Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import GlobalStyles from '../styles/global';
+import { MainContainer, MainContent } from '../styles/base';
+import PostTitle from '../components/PostTitle';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404</h1>
-    <p>Ops...</p>
-  </Layout>
-);
+function NotFoundPage() {
+  return (
+    <Layout>
+      <GlobalStyles />
+      <SEO title="404: Not found" />
+      <MainContainer>
+        <PostTitle title="404" />
+        <MainContent>
+          <h1>Ops</h1>
+        </MainContent>
+      </MainContainer>
+    </Layout>
+  );
+}
 
 export default NotFoundPage;
