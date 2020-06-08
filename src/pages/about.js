e76235f0b213b33772342311e8d0beec8b8b0473
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-06-07 23:09:41 -0300

    feat: Apply Layout to AboutPage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import SEO from '../components/SEO';
+import Layout from '../components/Layout';
+import GlobalStyles from '../styles/global';
+import { MainContainer, MainContent } from '../styles/base';
+import PostTitle from '../components/PostTitle';
 
 function AboutPage() {
-  return <h1>About Me</h1>;
+  return (
+    <Layout>
+      <GlobalStyles />
+      <SEO title="About" />
+      <MainContainer>
+        <PostTitle title="About me" />
+        <MainContent>
+          <p>Content</p>
+        </MainContent>
+      </MainContainer>
+    </Layout>
+  );
 }
 
 export default AboutPage;
