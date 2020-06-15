c7aae838538032f58823e1cd8e6274f0b1422f83
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-06-15 16:01:26 -0300

    feat: Update the Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,13 +78,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Code-With-Beer`,
+        short_name: `<Beer>`,
         start_url: `/`,
         background_color: `#131511;`,
         theme_color: `#131511`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/beer-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
